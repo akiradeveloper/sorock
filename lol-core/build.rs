0f87a5894001a08d8527e7d35a473d2908a96c77
Akira Hayakawa <ruby.wktk@gmail.com>
2020-09-16 07:18:49 +0000

    version: 0.1.3

--- lol-core/build.rs
+++ lol-core/build.rs
@@ -1,10 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let OUT_DIR = "proto_generated";
-    std::fs::create_dir(OUT_DIR);
-    tonic_build::configure()
-        .format(true)
-        .out_dir(OUT_DIR)
-        .compile(&["proto/lol-core.proto"], &["proto"])?;
+    // let OUT_DIR = "proto_generated";
+    // std::fs::create_dir(OUT_DIR);
+    // tonic_build::configure()
+    //     .format(true)
+    //     .out_dir(OUT_DIR)
+    //     .compile(&["proto/lol-core.proto"], &["proto"])?;
 
     tonic_build::compile_protos("proto/lol-core.proto")?;
 
