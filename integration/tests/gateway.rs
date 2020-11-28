1a425fc9bc6195bafd5d015b65bdebcc11257336
Akira Hayakawa <ruby.wktk@gmail.com>
2020-11-28 13:47:03 +0000

    rework connection

--- integration/tests/gateway.rs
+++ integration/tests/gateway.rs
@@ -1,5 +1,6 @@
 use integration::*;
-use lol_core::connection::{gateway, Connection, EndpointConfig};
+use lol_core::Id;
+use lol_core::connection::{self, gateway};
 use std::collections::HashSet;
 use std::time::Duration;
 
@@ -8,8 +9,7 @@ async fn test_gateway() {
     let env = init_cluster(1);
     let mut initial = HashSet::new();
     initial.insert(env.get_node_id(0).to_owned());
-    let gateway = gateway::Gateway::new(initial).await;
-    gateway::Gateway::start_companion_thread(&gateway).await;
+    let gateway = gateway::watch(initial);
 
     env.start(1, kvs_server(vec![]));
     env.start(2, kvs_server(vec![]));
@@ -17,26 +17,31 @@ async fn test_gateway() {
     Admin::to(0, env.clone()).add_server(2);
     tokio::time::delay_for(Duration::from_secs(6)).await;
 
-    let config = EndpointConfig::default();
-    let endpoints = gateway.read().await.query_sequence();
+    let connect = |id| async {
+        let endpoint = connection::Endpoint::from_shared(id).unwrap();
+        connection::connect(endpoint).await?;
+        Ok(())
+    };
+
+    let endpoints = gateway.borrow().list.clone();
     assert_eq!(endpoints.len(), 3);
-    let r = gateway::exec(&config, &endpoints, |_: Connection| async { Ok(()) }).await;
+    let r = gateway::exec(endpoints, |id: Id| connect(id)).await;
     assert!(r.is_ok());
 
     env.stop(0);
     env.stop(1);
     tokio::time::delay_for(Duration::from_secs(1)).await;
 
-    let endpoints = gateway.read().await.query_sequence();
-    let r = gateway::exec(&config, &endpoints, |_: Connection| async { Ok(()) }).await;
+    let endpoints = gateway.borrow().list.clone();
+    let r = gateway::exec(endpoints, |id: Id| connect(id)).await;
     // id=2 is alive
     assert!(r.is_ok());
 
     env.stop(2);
     tokio::time::delay_for(Duration::from_secs(1)).await;
 
-    let endpoints = gateway.read().await.query_sequence();
-    let r = gateway::exec(&config, &endpoints, |_: Connection| async { Ok(()) }).await;
+    let endpoints = gateway.borrow().list.clone();
+    let r = gateway::exec(endpoints, |id: Id| connect(id)).await;
     // all nodes are down
     assert!(r.is_err());
 }
