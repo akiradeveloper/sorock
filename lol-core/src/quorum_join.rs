cfd5c7fe59386ccd8f293f42587fe92641f2a2a2
Akira Hayakawa <ruby.wktk@gmail.com>
2020-11-30 03:56:11 +0000

    remove timeout parameter from quorum_join

--- lol-core/src/quorum_join.rs
+++ lol-core/src/quorum_join.rs
@@ -3,7 +3,8 @@ use std::time::Duration;
 use futures::stream::futures_unordered::FuturesUnordered;
 use futures::stream::StreamExt;
 
-async fn do_quorum_join(
+/// Execute futures and returns ok only when quorum replied ok.
+pub async fn quorum_join(
     quorum: usize,
     futs: Vec<impl Future<Output = bool> + Send + 'static>,
 ) -> bool {
@@ -38,52 +39,22 @@ async fn do_quorum_join(
     false
 }
 
-/// execute futures and returns ok only when quorum replied ok within specified duration.
-pub async fn quorum_join(
-    timeout: Duration,
-    quorum: usize,
-    futs: Vec<impl Future<Output = bool> + Send + 'static>,
-) -> bool {
-    let fut = do_quorum_join(quorum, futs);
-    let res = tokio::time::timeout(timeout, fut).await;
-    if res.is_err() {
-        false
-    } else {
-        res.unwrap()
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Duration;
-    use tokio::time::delay_for;
-    async fn f(b: bool, exec_sec: u64) -> bool {
-        delay_for(Duration::from_secs(exec_sec)).await;
+    async fn f(b: bool) -> bool {
         b
     }
     #[tokio::test]
     async fn test_quorum_join_ok() {
-        let futs = vec![f(true, 0), f(true, 0), f(false, 0)];
-        let ok = quorum_join(Duration::from_secs(1), 2, futs).await;
+        let futs = vec![f(true), f(false), f(true)];
+        let ok = quorum_join(2, futs).await;
         assert!(ok);
     }
     #[tokio::test]
     async fn test_quorum_join_ng() {
-        let futs = vec![f(true, 0), f(false, 0), f(false, 0)];
-        let ok = quorum_join(Duration::from_secs(1), 2, futs).await;
-        assert!(!ok);
-    }
-    #[tokio::test]
-    async fn test_quorum_join_wait_ok() {
-        let futs = vec![f(true, 2), f(true, 2), f(true, 4)];
-        let ok = quorum_join(Duration::from_secs(3), 2, futs).await;
-        assert!(ok);
-    }
-    #[tokio::test]
-    async fn test_quorum_join_timeout_ng() {
-        let futs = vec![f(true, 0), f(true, 2), f(true, 2)];
-        let ok = quorum_join(Duration::from_secs(1), 2, futs).await;
+        let futs = vec![f(true), f(false), f(false)];
+        let ok = quorum_join(2, futs).await;
         assert!(!ok);
     }
 }
