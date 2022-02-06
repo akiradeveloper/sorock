78cd6d64908582fc6508b6a4d17f8cc2d7bbda22
Akira Hayakawa <ruby.wktk@gmail.com>
2022-02-06 04:13:16 +0000

    Refactor tests

--- lol-core/src/storage/file.rs
+++ lol-core/src/storage/file.rs
@@ -129,48 +129,17 @@ async fn test_file_storage() -> anyhow::Result<()> {
 
 #[tokio::test]
 async fn test_file_storage_persistency() -> anyhow::Result<()> {
-    use super::RaftStorage;
-    use crate::Command;
-    use std::collections::HashSet;
-
     let _ = std::fs::create_dir("/tmp/lol");
     let path = Path::new("/tmp/lol/file2.db");
     Storage::destory(&path).unwrap();
     Storage::create(&path).unwrap();
+
     let s = Storage::open(&path);
+    super::persistency::test_pre_close(s).await?;
 
-    let e = Entry {
-        prev_clock: Clock { term: 0, index: 0 },
-        this_clock: Clock { term: 0, index: 0 },
-        command: Command::serialize(&Command::Noop),
-    };
-    let sn = Entry {
-        prev_clock: Clock { term: 0, index: 0 },
-        this_clock: Clock { term: 0, index: 0 },
-        command: Command::serialize(&Command::Snapshot {
-            membership: HashSet::new(),
-        }),
-    };
-    s.insert_entry(1, sn.clone()).await?;
-    s.insert_entry(2, e.clone()).await?;
-    s.insert_entry(3, e.clone()).await?;
-    s.insert_entry(4, e.clone()).await?;
-    s.insert_entry(3, sn.clone()).await?;
-
-    drop(s);
     let s = Storage::open(&path);
+    super::persistency::test_post_close(s).await?;
 
-    assert_eq!(
-        s.load_ballot().await?,
-        Ballot {
-            cur_term: 0,
-            voted_for: None
-        }
-    );
-    assert_eq!(super::find_last_snapshot_index(&s).await?, Some(3));
-    assert_eq!(s.get_last_index().await?, 4);
-
-    drop(s);
     Storage::destory(&path).unwrap();
     Ok(())
 }
