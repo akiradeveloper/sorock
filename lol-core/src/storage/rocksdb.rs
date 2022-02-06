ff998a1f5b485ddec80d028b8f7e04af98645cd9
Akira Hayakawa <ruby.wktk@gmail.com>
2022-02-06 12:03:46 +0000

    Add Result to storage APIs

--- lol-core/src/storage/rocksdb.rs
+++ lol-core/src/storage/rocksdb.rs
@@ -4,6 +4,7 @@ use rocksdb::{ColumnFamilyDescriptor, IteratorMode, Options, DB};
 use std::cmp::Ordering;
 use std::collections::BTreeSet;
 use std::path::{Path, PathBuf};
+use anyhow::Result;
 
 const CF_ENTRIES: &str = "entries";
 const CF_TAGS: &str = "tags";
@@ -27,11 +28,12 @@ fn comparator_fn(x: &[u8], y: &[u8]) -> Ordering {
 }
 
 impl Storage {
-    pub fn destroy(path: &Path) {
+    pub fn destroy(path: &Path) -> Result<()> {
         let opts = Options::default();
-        DB::destroy(&opts, path).unwrap();
+        DB::destroy(&opts, path)?;
+        Ok(())
     }
-    pub fn create(path: &Path) {
+    pub fn create(path: &Path) -> Result<()> {
         let mut db_opts = Options::default();
         db_opts.create_if_missing(true);
         db_opts.create_missing_column_families(true);
@@ -42,7 +44,7 @@ impl Storage {
             ColumnFamilyDescriptor::new(CF_TAGS, Options::default()),
             ColumnFamilyDescriptor::new(CF_CTRL, Options::default()),
         ];
-        let db = DB::open_cf_descriptors(&db_opts, path, cf_descs).unwrap();
+        let db = DB::open_cf_descriptors(&db_opts, path, cf_descs)?;
 
         let initial_ballot = Ballot {
             cur_term: 0,
@@ -50,9 +52,11 @@ impl Storage {
         };
         let cf = db.cf_handle(CF_CTRL).unwrap();
         let b: Vec<u8> = initial_ballot.into();
-        db.put_cf(&cf, BALLOT, b).unwrap();
+        db.put_cf(&cf, BALLOT, b)?;
+
+        Ok(())
     }
-    fn open_db(path: &Path) -> DB {
+    fn open_db(path: &Path) -> Result<DB> {
         let db_opts = Options::default();
         let mut opts = Options::default();
         opts.set_comparator(CMP, comparator_fn);
@@ -61,19 +65,19 @@ impl Storage {
             ColumnFamilyDescriptor::new(CF_TAGS, Options::default()),
             ColumnFamilyDescriptor::new(CF_CTRL, Options::default()),
         ];
-        DB::open_cf_descriptors(&db_opts, path, cf_descs).unwrap()
+        let db = DB::open_cf_descriptors(&db_opts, path, cf_descs)?;
+        Ok(db)
     }
-    pub fn open(path: &Path) -> Storage {
-        let db = Self::open_db(path);
-        Self {
+    pub fn open(path: &Path) -> Result<Storage> {
+        let db = Self::open_db(path)?;
+        Ok(Self {
             db
-        }
+        })
     }
 }
 pub struct Storage {
     db: DB,
 }
-use anyhow::Result;
 #[async_trait::async_trait]
 impl super::RaftStorage for Storage {
     async fn delete_entry(&self, i: Index) -> Result<()> {
@@ -134,13 +138,13 @@ impl super::RaftStorage for Storage {
 async fn test_rocksdb_storage() -> Result<()> {
     let _ = std::fs::create_dir("/tmp/lol");
     let path = Path::new("/tmp/lol/disk1.db");
-    Storage::destroy(path);
-    Storage::create(path);
+    Storage::destroy(path)?;
+    Storage::create(path)?;
 
-    let s = Storage::open(path);
+    let s = Storage::open(path)?;
     super::test_storage(s).await?;
 
-    Storage::destroy(path);
+    Storage::destroy(path)?;
     Ok(())
 }
 
@@ -148,15 +152,15 @@ async fn test_rocksdb_storage() -> Result<()> {
 async fn test_rocksdb_persistency() -> Result<()> {
     let _ = std::fs::create_dir("/tmp/lol");
     let path = Path::new("/tmp/lol/disk2.db");
-    Storage::destroy(path);
-    Storage::create(path);
+    Storage::destroy(path)?;
+    Storage::create(path)?;
 
-    let s = Storage::open(path);
+    let s = Storage::open(path)?;
     super::persistency::test_pre_close(s).await?;
 
-    let s = Storage::open(path);
+    let s = Storage::open(path)?;
     super::persistency::test_post_close(s).await?;
 
-    Storage::destroy(path);
+    Storage::destroy(path)?;
     Ok(())
 }
