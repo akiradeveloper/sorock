eed62377b227f81aeaf5af6410ad30b65615e57e
Akira Hayakawa <ruby.wktk@gmail.com>
2022-12-08 06:25:31 +0000

    Rename lol-core.proto to lol_core.proto

--- lol-core/build.rs
+++ lol-core/build.rs
@@ -1,17 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let OUT_DIR = "proto_generated";
-    // std::fs::create_dir(OUT_DIR);
-    // tonic_build::configure()
-    //     .format(true)
-    //     .out_dir(OUT_DIR)
-    //     .compile(&["proto/lol-core.proto"], &["proto"])?;
-
     let mut config = prost_build::Config::new();
     config.bytes(&[
         ".lol_core.AppendStreamEntry.command",
         ".lol_core.GetSnapshotRep.chunk",
     ]);
-    tonic_build::configure().compile_with_config(config, &["proto/lol-core.proto"], &["proto"])?;
+    tonic_build::configure().compile_with_config(config, &["proto/lol_core.proto"], &["proto"])?;
 
     Ok(())
 }
