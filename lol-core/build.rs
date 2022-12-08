531af205f6a45580539aa20abea67646f585124d
Akira Hayakawa <ruby.wktk@gmail.com>
2022-12-08 10:48:45 +0000

    [work-around] Don't compile in docs.rs

--- lol-core/build.rs
+++ lol-core/build.rs
@@ -1,13 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = prost_build::Config::new();
-    config.bytes(&[
-        ".lol_core.AppendStreamEntry.command",
-        ".lol_core.GetSnapshotRep.chunk",
-    ]);
-    // Output the generated rs files to `src/proto/`
-    tonic_build::configure()
-        .out_dir("src/proto/")
-        .compile_with_config(config, &["lol_core.proto"], &["proto"])?;
-
+    if std::env::var("DOCS_RS").is_ok() {
+        let mut config = prost_build::Config::new();
+        config.bytes(&[
+            ".lol_core.AppendStreamEntry.command",
+            ".lol_core.GetSnapshotRep.chunk",
+        ]);
+        // Output the generated rs files to `src/proto/`
+        tonic_build::configure()
+            .out_dir("src/proto/")
+            .compile_with_config(config, &["lol_core.proto"], &["proto"])?;
+    }
     Ok(())
 }
