d0a390608136c5b584b4af23dcf4d880c7781263
Akira Hayakawa <ruby.wktk@gmail.com>
2024-02-29 09:30:40 +0900

    failure-detector: Remove unused functions

--- lol2/src/process/voter/failure_detector.rs
+++ lol2/src/process/voter/failure_detector.rs
@@ -26,23 +26,12 @@ impl FailureDetector {
         }
     }
 
-    fn get_watch_id(&self) -> NodeId {
-        self.inner.lock().unwrap().watch_id.clone()
-    }
-
     pub fn receive_heartbeat(&self, leader_id: NodeId) {
         let cur_watch_id = self.inner.lock().unwrap().watch_id.clone();
         if cur_watch_id != leader_id {
             *self.inner.lock().unwrap() = Inner::watch(leader_id);
         }
         self.inner.lock().unwrap().detector.add_ping(Instant::now())
-    }
-
-    fn detect_election_timeout(&self) -> bool {
-        let fd = &self.inner.lock().unwrap().detector;
-        let normal_dist = fd.normal_dist();
-        let phi = normal_dist.phi(Instant::now() - fd.last_ping());
-        phi > 3.
     }
 
     pub fn get_election_timeout(&self) -> Option<Duration> {
