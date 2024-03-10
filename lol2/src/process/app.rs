39a3b331654e155e8d530698379794b5ba7be33b
Akira Hayakawa <ruby.wktk@gmail.com>
2024-03-11 07:44:53 +0900

    Add comment

--- lol2/src/process/app.rs
+++ lol2/src/process/app.rs
@@ -9,19 +9,21 @@ impl App {
 
     pub async fn fetch_snapshot(
         &self,
-        index: Index,
+        snapshot_index: Index,
         owner: NodeId,
         driver: RaftDriver,
     ) -> Result<()> {
         if owner == driver.self_node_id() {
             return Ok(());
         }
-        if index < 2 {
+        // Every node should have a snapshot at index 1
+        // because it is the initial state of the application.
+        if snapshot_index == 1 {
             return Ok(());
         }
         let conn = driver.connect(owner);
-        let st = conn.get_snapshot(index).await?;
-        self.save_snapshot(st, index).await?;
+        let st = conn.get_snapshot(snapshot_index).await?;
+        self.save_snapshot(st, snapshot_index).await?;
         Ok(())
     }
 }
