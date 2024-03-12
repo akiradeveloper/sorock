c1b6fce647375b2838ffc293f9b0a3e62c3ceb1b
Akira Hayakawa <ruby.wktk@gmail.com>
2024-03-12 14:28:35 +0900

    Add test

---
+++ tests/lol-tests/tests/reflection.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+use lol_tests::*;
+use serial_test::serial;
+use test_log::test;
+
+#[serial]
+#[test(tokio::test(flavor = "multi_thread"))]
+async fn grpcurl() -> Result<()> {
+    let cluster = Cluster::new(1).await?;
+
+    let address = {
+        let raw: tonic::transport::Uri = env::address_from_id(0).parse()?;
+        let out = format!("{}", raw.authority().unwrap());
+        dbg!(&out);
+        out
+    };
+
+    let out = std::process::Command::new("grpcurl")
+        .arg("-plaintext")
+        .arg(address)
+        .arg("list")
+        .output()?;
+
+    assert!(out.status.success());
+    dbg!(String::from_utf8(out.stdout)?);
+
+    Ok(())
+}
