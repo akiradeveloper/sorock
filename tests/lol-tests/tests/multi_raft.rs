e44fedad2b7336ac9952e29f13adb2ac7582a622
Akira Hayakawa <ruby.wktk@gmail.com>
2024-05-25 08:44:34 +0000

    Change test params

--- tests/lol-tests/tests/multi_raft.rs
+++ tests/lol-tests/tests/multi_raft.rs
@@ -7,8 +7,8 @@ use std::sync::Arc;
 
 #[serial]
 #[tokio::test(flavor = "multi_thread")]
-async fn N3_L300_K3_multi_raft_cluster() -> Result<()> {
-    const L: u32 = 300;
+async fn N3_L1000_K3_multi_raft_cluster() -> Result<()> {
+    const L: u32 = 1000;
     let cluster = Arc::new(Cluster::new(3, L).await?);
 
     let mut futs = vec![];
@@ -37,8 +37,8 @@ async fn N3_L300_K3_multi_raft_cluster() -> Result<()> {
 
 #[serial]
 #[tokio::test(flavor = "multi_thread")]
-async fn N3_L100_K3_multi_raft_io_roundrobin() -> Result<()> {
-    const L: u32 = 100;
+async fn N3_L1000_K3_multi_raft_io_roundrobin() -> Result<()> {
+    const L: u32 = 1000;
 
     let cluster = Arc::new(Cluster::new(3, L).await?);
 
@@ -70,7 +70,7 @@ async fn N3_L100_K3_multi_raft_io_roundrobin() -> Result<()> {
 
 #[serial]
 #[tokio::test(flavor = "multi_thread")]
-async fn N1_L300_K3_multi_raft_io() -> Result<()> {
+async fn N1_L1000_K3_multi_raft_io() -> Result<()> {
     const L: u32 = 1000;
     let cluster = Arc::new(Cluster::new(1, L).await?);
 
@@ -99,8 +99,8 @@ async fn N1_L300_K3_multi_raft_io() -> Result<()> {
 
 #[serial]
 #[tokio::test(flavor = "multi_thread")]
-async fn N3_L100_K3_multi_raft_io() -> Result<()> {
-    const L: u32 = 100;
+async fn N3_L1000_K3_multi_raft_io() -> Result<()> {
+    const L: u32 = 1000;
 
     let cluster = Arc::new(Cluster::new(3, L).await?);
 
