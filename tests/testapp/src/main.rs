f90da7e8eb5bf7ce40b34fe6243938913b8c1000
Akira Hayakawa <ruby.wktk@gmail.com>
2024-05-25 02:24:50 +0000

    Use tokio-soncole for debugging

--- tests/testapp/src/main.rs
+++ tests/testapp/src/main.rs
@@ -34,6 +34,11 @@ struct EnvConfig {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    console_subscriber::ConsoleLayer::builder()
+        .retention(std::time::Duration::from_secs(60))
+        .server_addr(([0, 0, 0, 0], 6669))
+        .init();
+
     env_logger::init();
 
     let env_config: EnvConfig = envy::from_env()?;
