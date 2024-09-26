fe0114f315753a47ceabbe0df1a2fafcdb3a8525
Akira Hayakawa <ruby.wktk@gmail.com>
2024-09-26 20:20:42 +0900

    Remove lol-benches and move to lol-tests

--- lol/benches/insert.rs
+++ lol/benches/insert.rs
@@ -18,7 +18,7 @@ fn do_bench(n: usize, b: &mut test::Bencher) {
 
     let mem = redb::backends::InMemoryBackend::new();
     let db = redb::Database::builder().create_with_backend(mem).unwrap();
-    let db = lol::backend::Backend::new(db);
+    let db = lol::backends::redb::Backend::new(db);
     let (log, _) = db.get(0).unwrap();
 
     let mut i = 0;
