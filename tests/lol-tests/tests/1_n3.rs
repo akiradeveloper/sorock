fa35249dfd2ca695a9824a0ff94e09ad5d465279
Akira Hayakawa <ruby.wktk@gmail.com>
2024-09-26 11:52:44 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- tests/lol-tests/tests/1_n3.rs
+++ tests/lol-tests/tests/1_n3.rs
@@ -54,7 +54,7 @@ async fn n3_par_write() -> Result<()> {
     }
     futures::future::try_join_all(futs).await?;
 
-    // let expected = cluster.user(1).read(0).await?;
+    let expected = cluster.user(1).read(0).await?;
     // assert_eq!(expected, N);
 
     Ok(())
