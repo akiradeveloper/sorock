ab38c43adf2b9f06871df5d13788cfa9bb428abe
Akira Hayakawa <ruby.wktk@gmail.com>
2024-10-19 09:44:46 +0900

    fmt

--- tests/testapp/build.rs
+++ tests/testapp/build.rs
@@ -1,6 +1,4 @@
 fn main() {
     let fds = protox::compile(&["ping.proto"], &["proto"]).unwrap();
-    tonic_build::configure()
-        .compile_fds(fds)
-        .unwrap();
+    tonic_build::configure().compile_fds(fds).unwrap();
 }
