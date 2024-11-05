053b3909460b427d73c88c82936b0851299fbb34
Akira Hayakawa <ruby.wktk@gmail.com>
2024-11-05 07:41:48 +0000

    Channel length 0 is enough to notify drop

--- sorock/src/backend/redb/mod.rs
+++ sorock/src/backend/redb/mod.rs
@@ -20,7 +20,7 @@ impl Backend {
         let db = Arc::new(redb);
 
         let (reaper, tx) = log::Reaper::new(db.clone());
-        let (_kill_tx, kill_rx) = crossbeam::channel::bounded(1);
+        let (_kill_tx, kill_rx) = crossbeam::channel::bounded(0);
         std::thread::spawn(move || loop {
             if let Err(TryRecvError::Disconnected) = kill_rx.try_recv() {
                 break;
