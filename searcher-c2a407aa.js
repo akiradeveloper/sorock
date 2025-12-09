3d02b81109dad44243f889bec03d29895b654130
akiradeveloper <akiradeveloper@users.noreply.github.com>
2025-12-09 01:57:12 +0000

    deploy: c078fe76884d5984aaa5cdda04329ec188e91ea6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e6fac20a.js',
+                path_to_root + 'searchindex-dbe9e845.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
