e1e82871d8b0a538cd0057cb71e7d44715d2984a
Akira Hayakawa <ruby.wktk@gmail.com>
2025-12-12 09:05:48 +0900

    Factor out LogStorage

---
+++ sorock/src/log_storage/mod.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use crossbeam::channel::TryRecvError;
+use std::sync::Arc;
+use std::time::Duration;
+
+mod reaper;
+mod view;
+
+use reaper::{LazyInsert, Reaper, ReaperTx};
+pub use view::LogShardView;
+
+pub struct LogStorage {
+    db: Arc<redb::Database>,
+    tx: reaper::ReaperTx,
+    _kill_tx: crossbeam::channel::Sender<()>,
+}
+
+impl LogStorage {
+    pub fn new(db: Arc<redb::Database>) -> Self {
+        let (reaper, tx) = Reaper::new(db.clone());
+        let (_kill_tx, kill_rx) = crossbeam::channel::bounded(0);
+        std::thread::spawn(move || loop {
+            if let Err(TryRecvError::Disconnected) = kill_rx.try_recv() {
+                break;
+            }
+            reaper.reap().ok();
+        });
+
+        Self { db, tx, _kill_tx }
+    }
+
+    pub fn get_shard(&self, shard_index: u32) -> Result<LogShardView> {
+        Ok(LogShardView::new(
+            self.db.clone(),
+            shard_index,
+            self.tx.clone(),
+        )?)
+    }
+}
+
+fn table_def(space: &str) -> redb::TableDefinition<'_, u64, Vec<u8>> {
+    redb::TableDefinition::new(space)
+}
