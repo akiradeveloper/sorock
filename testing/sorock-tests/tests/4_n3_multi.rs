0005e36b9df5959e59c572ef711cdf2ec721d4e4
Akira Hayakawa <ruby.wktk@gmail.com>
2025-12-27 20:53:59 +0900

    fix test

--- testing/sorock-tests/tests/4_n3_multi.rs
+++ testing/sorock-tests/tests/4_n3_multi.rs
@@ -23,7 +23,7 @@ async fn n3_p30_multi_raft_cluster() -> Result<()> {
             cluster
                 .admin(leader)
                 .send_timeout_now(TimeoutNow { shard_id })
-                .await?;
+                .await.ok();
 
             Ok::<(), anyhow::Error>(())
         };
